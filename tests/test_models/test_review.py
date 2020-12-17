bf07e9481acaec01ae264027a4a5fae2678cabe2
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-17 16:56:49 -0500

    updating tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,28 +2,38 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+from os import getenv
 
 
 class test_review(test_basemodel):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+    if getenv("HBNB_TYPE_STORAGE") != "db":
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        def __init__(self, *args, **kwargs):
+            """ """
+            super().__init__(*args, **kwargs)
+            self.name = "Review"
+            self.value = Review
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        def test_place_id(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.place_id), str)
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        def test_user_id(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.user_id), str)
+
+        def test_text(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.text), str)
+    else:
+        def test_review_attributes(self):
+            """ """
+            new = Review(text="nice", place_id="001", user_id="007")
+            self.assertEqual(type(new.place_id), str)
+            self.assertEqual(type(new.user_id), str)
+            self.assertEqual(type(new.text), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,47 @@
+  
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+from os import getenv
 
 
 class test_User(test_basemodel):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    if getenv("HBNB_TYPE_STORAGE") != "db":
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        def __init__(self, *args, **kwargs):
+            """ """
+            super().__init__(*args, **kwargs)
+            self.name = "User"
+            self.value = User
 
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        def test_first_name(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.first_name), str)
 
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+        def test_last_name(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.last_name), str)
 
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+        def test_email(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.email), str)
+
+        def test_password(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.password), str)
+    else:
+        def test_user_attributes(self):
+            """ """
+            new = User(first_name="John", last_name="Doe",
+                       email="john@example.com", password="password")
+            self.assertEqual(type(new.first_name), str)
+            self.assertEqual(type(new.last_name), str)
+            self.assertEqual(type(new.email), str)
+            self.assertEqual(type(new.password), str)
