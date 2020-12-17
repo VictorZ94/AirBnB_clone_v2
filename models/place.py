4f9908c24e63b872bff3db8be029cc7fb6ddcde2
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-17 13:13:26 -0500

    updating asociation

--- models/place.py
+++ models/place.py
@@ -5,6 +5,19 @@ from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
+asociation = Table('place_amenity', Base.metadata,
+                   Column('place_id',
+                          String(60),
+                          ForeignKey("places.id"),
+                          primary_key=True,
+                          nullable=False),
+                   Column('amenity_id',
+                          String(60),
+                          ForeignKey("amenities.id"),
+                          primary_key=True,
+                          nullable=False)
+                   )
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
