def9004ce55d7fb5a25442daa85e490e1bcbdca3
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-17 16:49:21 -0500

    adding tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-""" """
+""" Amenity tests """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+from os import getenv
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ Class tests """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        def __init__(self, *args, **kwargs):
+            """ """
+            super().__init__(*args, **kwargs)
+            self.name = "Amenity"
+            self.value = Amenity
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        def test_name2(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.name), str)
+    else:
+        def test_name2(self):
+            """ """
+            new = Amenity(name="cool")
+            self.assertEqual(type(new.name), str)
