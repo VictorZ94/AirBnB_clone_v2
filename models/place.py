45ae2598c2689502bf9bc680c198c2c1d7cfe0d2
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-17 13:15:20 -0500

    hot fix

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
