d348e94ec85b7b71d97ef0642a1568922a54f138
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-18 01:31:53 -0500

    fixxing test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Testing basemodel module """
+""" """
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -9,90 +9,94 @@ import os
 
 
 class test_basemodel(unittest.TestCase):
-    """Testing BaseModel class """
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """Initializes data"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
+        self.base = self.value()
 
     def setUp(self):
-        """setting up """
+        """ """
         pass
 
     def tearDown(self):
-        """remove json file"""
         try:
             os.remove('file.json')
         except:
             pass
 
     def test_default(self):
-        """default test """
-        i = self.value()
-        self.assertEqual(type(i), self.value)
+        """ """
+        i = self.base
+        self.assertEqual(type(i), BaseModel)
 
     def test_kwargs(self):
-        """testing kwargs"""
-        i = self.value()
+        """ """
+        i = self.base
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """testing kwargs int """
-        i = self.value()
+        """ """
+        i = self.base
         copy = i.to_dict()
         copy.update({1: 2})
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == "db",
-                     "only test for file storage")
     def test_save(self):
         """ Testing save """
-        i = self.value()
+        i = self.base
         i.save()
-        key = self.name + "." + i.id
+        key = "BaseModel" + "." + i.id
         with open('file.json', 'r') as f:
             j = json.load(f)
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """testing strings"""
-        i = self.value()
+        """ """
+        i = self.base
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """Testing to_dict method"""
-        i = self.value()
+        """ """
+        i = self.base
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """Testing kwargs none"""
+        """ """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
+    def test_kwargs_one(self):
+        """ """
+        n = {'Name': 'test'}
+        new = self.value(**n)
+        self.assertEqual(new.Name, 'test')
+
     def test_id(self):
-        """Testing id attribute"""
-        new = self.value()
+        """ """
+        new = self.base
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """Testing created_at attribute"""
-        new = self.value()
+        """ """
+        new = self.base
         self.assertEqual(type(new.created_at), datetime.datetime)
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == "db",
-                     "only test for file storage")
     def test_updated_at(self):
-        """Testing updated_at attribute"""
-        new = self.value()
+        """ """
+        new = self.base
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
+        print(new.created_at)
+        print(new.updated_at)
         self.assertFalse(new.created_at == new.updated_at)
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,8 +6,6 @@ from models import storage
 import os
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == "db",
-                 "only test for db storage, not file storage")
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
