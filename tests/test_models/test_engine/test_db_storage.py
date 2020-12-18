709593d002ad3a08f279a3e4627d8f8400db8901
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-17 19:42:02 -0500

    Revert "fixxing test"
    This reverts commit 940103d418912a126a09e4efcb15d700b99b3b8b.

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
-"""test for file storage"""
+""" Module for testing db storage"""
 import unittest
-import pep8
+from models.base_model import BaseModel
+from models import storage
+from models.base_model import Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.engine.db_storage import DBStorage
+import os
+import MySQLdb
 
 
-class TestDBStorage(unittest.TestCase):
-    '''Tests the DBStorage storage engine'''
+tbl_cls = {"states": State, "cities": City, "places": Place,
+           "amenities": Amenity, "users": User, "reviews": Review}
 
-    def setUp(self):
-        """SetUp env for test"""
-        pass
 
-    def tearDown(self):
-        """teardown"""
-        pass
+class test_dbStorage(unittest.TestCase):
+    """Tests for db storage mode"""
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != "db",
+                     "only test for db storage, not file storage")
+    def test_doc(self):
+        """Test docstrings"""
+        self.assertIsNotNone(DBStorage.__doc__)
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != "db",
+                     "only test for db storage, not file storage")
     def test_all(self):
-        """Test class method all()"""
-        pass
+        """Test all method"""
+        d = storage.all()
+        self.assertTrue(type(d), dict)
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != "db",
+                     "only test for db storage, not file storage")
     def test_new(self):
         """Test new method"""
-        pass
-
-    def test_save(self):
-        """Test save method
-        """
-        pass
-
-    def test_delete(self):
-        """Tests delete
-        """
-        pass
-
-    def test_reload(self):
-        """Tests reload
-        """
-        pass
-
-if __name__ == "__main__":
-    unittest.main()
+        db = MySQLdb.connect(user=os.getenv('HBNB_MYSQL_USER'),
+                             passwd=os.getenv('HBNB_MYSQL_PWD'),
+                             db=os.getenv('HBNB_MYSQL_DB'))
+        cur = db.cursor()
+        for tbl in tbl_cls:
+            rows = cur.execute("SELECT COUNT(*) FROM {}".format(tbl))
+            obj1 = tbl_cls[tbl]()
+            storage.new(obj1)
+            rows_final = cur.execute("SELECT COUNT(*) FROM {}".format(tbl))
+            self.assertTrue(rows + 1, rows_final)
+        cur.close()
+        db.close()
