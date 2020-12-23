2c074799f57d4e115284b32a2dc3e4742e03a077
Victor Zuluaga <zrvictor@outlook.com>
2020-12-22 20:55:12 -0500

    pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(name))
+        return name
+    except:
+        return None
