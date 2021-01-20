685103146c4c06d39d516aea7e603e75d35b6aed
Victor Zuluaga <zrvictor@outlook.com>
2021-01-20 17:54:14 -0500

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,9 +19,8 @@ def app_index():
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     states = storage.all(State).values()
-    cities = storage.all(City).values()
     return render_template('8-cities_by_states.html',
-                           states=states, cities=cities)
+                           states=states)
 
 
 @app.teardown_appcontext
