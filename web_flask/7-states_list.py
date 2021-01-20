f270283ca833dc4ed8afdc5d2dd9902414341693
Victor Zuluaga <zrvictor@outlook.com>
2021-01-20 15:57:41 -0500

    display states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,15 +11,8 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def app_index():
-    list_id = []
-    list_name = []
-    for value in storage.all(State).values():
-        list_id.append(value.id)
-        list_name.append(value.name)
-    return render_template('7-states_list.html',
-                           len=len(list_id),
-                           list_id=list_id,
-                           list_name=list_name)
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
