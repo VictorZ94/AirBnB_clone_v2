b53d12c0da4db9ffd07dd4c0a0c8bf63afd74192
Victor Zuluaga <zrvictor@outlook.com>
2021-01-19 20:58:15 -0500

    using template in flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -45,7 +45,7 @@ def only_int(n):
 
 @app.route('/number_template/<int:n>')
 def only_template_int(n):
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
