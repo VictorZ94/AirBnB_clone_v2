fe60e9c6c83e84a705ae63b4f9529d41699d33c9
Victor Zuluaga <zrvictor@outlook.com>
2021-01-20 15:03:58 -0500

    fetch data of db and display in html template

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""[summary]
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def app_index():
+    list_id = []
+    list_name = []
+    for value in storage.all(State).values():
+        list_id.append(value.id)
+        list_name.append(value.name)
+    return render_template('7-states_list.html',
+                           len=len(list_id),
+                           list_id=list_id,
+                           list_name=list_name)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
