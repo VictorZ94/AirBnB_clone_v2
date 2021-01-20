e71339ebb39088e2af1c452fc4d5bbda6720adab
Victor Zuluaga <zrvictor@outlook.com>
2021-01-20 18:51:19 -0500

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,12 +12,16 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def app_index():
+    """ function to call all states
+    """
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
+    """ function to call all cities
+    """
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html',
                            states=states)
@@ -25,6 +29,8 @@ def cities_by_states():
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """ After each request remove the current SQLAlchemy Session:
+    """
     storage.close()
 
 
